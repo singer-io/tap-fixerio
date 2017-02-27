32f6347e7d5c3e6e518026223e477a9db7dee3d8
Christopher Merrick <chris@stitchdata.com>
2017-02-27 18:15:16 -0500

    use state

--- tap_fixerio.py
+++ tap_fixerio.py
@@ -4,18 +4,21 @@ import json
 import sys
 import argparse
 import time
-
 import requests
 import singer
+
+from datetime import date, datetime, timedelta
 
 base_url = 'https://api.fixer.io/'
 
 logger = singer.get_logger()
 
+DATE_FORMAT='%Y-%m-%d'
+
 def parse_response(r):
     flattened = r['rates']
     flattened[r['base']] = 1.0
-    flattened['date'] = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.strptime(r['date'], '%Y-%m-%d'))
+    flattened['date'] = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.strptime(r['date'], DATE_FORMAT))
     return flattened
 
 schema = {'type': 'object',
@@ -24,28 +27,37 @@ schema = {'type': 'object',
                     'format': 'date-time'}},
           'additionalProperties': True}
 
-def do_sync(args):
-
-    if args.config:
-        with open(args.config) as file:
-            config = json.load(file)
-    else:
-        config = {}
-
-    base = config.get('base', 'USD')
-    logger.info('Replicating the latest exchange rate data from fixer.io')
+def do_sync(base, start_date):
+    logger.info('Replicating exchange rate data from fixer.io starting from {}'.format(start_date))
     singer.write_schema('exchange_rate', schema, 'date')
 
+    state = {'start_date': start_date}
+    next_date = start_date
+    params = {'base': base}
+    
     try:
-        params = {'base': base}
-        response = requests.get(url=base_url + '/latest', params=params)
-        singer.write_records('exchange_rate', [parse_response(response.json())])
-        logger.info('Tap exiting normally')
+        while True:
+            response = requests.get(url=base_url + next_date, params=params)
+            response.raise_for_status()
+            
+            payload = response.json()
+
+            if datetime.strptime(next_date, DATE_FORMAT) > datetime.utcnow():
+                break
+            else:
+                singer.write_records('exchange_rate', [parse_response(payload)])
+                state = {'start_date': next_date}
+                next_date = (datetime.strptime(next_date, DATE_FORMAT) + timedelta(days=1)).strftime(DATE_FORMAT)
+            
     except requests.exceptions.RequestException as e:
         logger.fatal('Error on ' + e.request.url +
                      '; received status ' + str(e.response.status_code) +
                      ': ' + e.response.text)
+        singer.write_state(state)
         sys.exit(-1)
+
+    singer.write_state(state)
+    logger.info('Tap exiting normally')
 
 
 def main():
@@ -57,8 +69,23 @@ def main():
         '-s', '--state', help='State file', required=False)
 
     args = parser.parse_args()
+    
+    if args.config:
+        with open(args.config) as file:
+            config = json.load(file)
+    else:
+        config = {}
 
-    do_sync(args)
+    if args.state:
+        with open(args.state) as file:
+            state = json.load(file)
+    else:
+        state = {}
+
+    start_date = state.get('start_date',
+                           config.get('start_date', datetime.utcnow().strftime(DATE_FORMAT)))
+    
+    do_sync(config.get('base', 'USD'), start_date)
 
 
 if __name__ == '__main__':
